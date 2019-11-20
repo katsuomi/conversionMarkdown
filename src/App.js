668fd533426591e25f422bed05b7aedfcef6e8d9
katsuomi <mugichak1227@gmail.com>
2019-11-20 14:28:20 +0900

    update view

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import ReactMarkdown from 'react-markdown'
+import marked from 'marked'
 
-function App() {
+const App = () => {
+  const [inputContent, setInputContent] = useState('');
+  // console.log(inputContent) // markdown変換
+  console.log(marked(inputContent)) //html変換
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Grid container>
+      <Grid item xs={6}>
+      <TextField
+        id="outlined-multiline-static"
+        label="markdown"
+        multiline
+        rows="300"
+        margin="normal"
+        variant="outlined"
+        style={{width: "100%"}}
+        onChange={(e) => {setInputContent(e.target.value)}}
+      />
+      </Grid>
+      <Grid item xs={6} style={{paddingLeft: "20px"}}>
+        <ReactMarkdown
+          source={inputContent}
+          escapeHtml={false}
+        />
+      </Grid>
+    </Grid>
+  )
 }
 
-export default App;
+export default App
